fc591d073f2de1f5fec6a5695b0ca8b3bb9e7a51
Anmol Sharma <anmol.sharma293@gmail.com>
2018-06-04 14:13:08 -0700

    script changes

--- gcp_startup_script.sh
+++ gcp_startup_script.sh
@@ -1,54 +1,11 @@
-set -v
-
-# Talk to the metadata server to get the project id
-PROJECTID=$(curl -s "http://metadata.google.internal/computeMetadata/v1/project/project-id" -H "Metadata-Flavor: Google")
-
-# Install logging monitor. The monitor will automatically pickup logs sent to
-# syslog.
-curl -s "https://storage.googleapis.com/signals-agents/logging/google-fluentd-install.sh" | bash
-service google-fluentd restart &
-
-# Install dependencies from apt
-apt-get update
-apt-get install -yq \
-    git build-essential gcc supervisor python python-dev python-pip libffi-dev \
-    libssl-dev libfreeimage3 libfreeimage-dev
-
-# Create a pythonapp user. The application will run as this user.
-useradd -m -d /home/pythonapp pythonapp
-
-# pip from apt is out of date, so make it update itself and install virtualenv.
-pip install --upgrade pip virtualenv
-
-# git requires $HOME and it's not set during the startup script.
-export HOME=/root
-
-# Install app dependencies
-cd /opt/app
-virtualenv /opt/app/gui-proj/env
-/opt/app/gui-proj/env/bin/pip install -r /opt/app/gui-proj/requirements.txt
-
-# Make sure the pythonapp user owns the application code
-chown -R pythonapp:pythonapp /opt/app/gui-proj/
-
-# Configure supervisor to start gunicorn inside of our virtualenv and run the
-# application.
-cat >/etc/supervisor/conf.d/python-app.conf << EOF
-[program:pythonapp]
-directory=/opt/app/gui-proj/
-command=/opt/app/gui-proj/env/bin/gunicorn mri-seg:app --bind 0.0.0.0:8080
-autostart=true
-autorestart=true
-user=pythonapp
-# Environment variables ensure that the application runs inside of the
-# configured virtualenv.
-environment=VIRTUAL_ENV="/opt/app/gui-proj/env",PATH="/opt/app/gui-proj/env/bin",\
-    HOME="/home/pythonapp",USER="pythonapp"
-stdout_logfile=syslog
-stderr_logfile=syslog
-EOF
-
-supervisorctl reread
-supervisorctl update
-
-# Application should now be running under supervisor
+sudo apt install python-dev python-pip build-essential gcc git
+pip install virtualenvwrapper --user
+echo 'export WORKON_HOME=$HOME/.virtualenvs' >> ~/.bashrc
+echo 'export PROJECT_HOME=$HOME/Devel' >> ~/.bashrc
+echo 'source $HOME/.local/bin/virtualenvwrapper.sh' >> ~/.bashrc
+echo 'export PATH="$PATH:/home/anmol_sharma293/.local/bin/"' >> ~/.bashrc
+source ~/.bashrc
+mkvirtualenv gui-proj
+workon gui-proj
+pip install -r requirements.txt
+sudo pip install git+https://www.github.com/keras-team/keras-contrib.git
--- transfer_to_gcp.sh
+++ transfer_to_gcp.sh
@@ -1,2 +1,2 @@
-tar -cvf ../gui-proj.tar ../gui-proj/*
+tar -cvf ../gui-proj.tar ../gui-proj/.
 scp -i ~/.ssh/id_rsa ../gui-proj.tar anmol@35.237.3.223:/home/anmol
